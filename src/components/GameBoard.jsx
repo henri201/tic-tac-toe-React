25e721547a7c1c4c0190da010a6c46e6ea22a264
Henri <64684431+henri201@users.noreply.github.com>
2024-03-26 19:07:49 +0200

    Update GameBoard.jsx

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -9,7 +9,7 @@ export default function GameBoard({onSelectSquare, turns}) {
 
   let gameBoard = initialGameBoard;
 
-  for (const turn of turns) {
+  for (const turn of turns) {        //bug here , doesnt iterate
     const {square, player} = turn;
     const {row, col} = square;
 
